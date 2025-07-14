f596817bb28f1cf7987bfd9df8f736a09ce7733d
night9a <nightxcros@gmail.com>
2025-07-14 09:31:33 +0100

    Initial commit

---
+++ src/main.rs
@@ -0,0 +1,17 @@
+#[macro_use] extern crate rocket;
+
+use rocket_dyn_templates::Template;
+use std::collections::HashMap;
+
+#[get("/")]
+fn index() -> Template {
+    let context: HashMap<&str, &str> = HashMap::new(); // فارغ
+    Template::render("index", &context)
+}
+
+#[launch]
+fn rocket() -> _ {
+    rocket::build()
+        .mount("/", routes![index])
+        .attach(Template::fairing())
+}
